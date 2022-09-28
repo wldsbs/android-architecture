b6e1c166c655bea8fc6dd793373431d10c760a8f
jiyun <jiyun_99@naver.com>
2022-09-28 20:23:33 +0900

    Revert "mvc"
    This reverts commit 907c532a411b76ddac06f0cc964c4216759511e4.

--- app/src/main/java/com/example/android_architecture/MainActivity.kt
+++ app/src/main/java/com/example/android_architecture/MainActivity.kt
@@ -19,3 +19,35 @@ class MainActivity : AppCompatActivity() {
         textView.text = model.getData()
     }
 }
+
+//class MainActivity : AppCompatActivity(), Presenter.View {
+//    private lateinit var presenter: Presenter
+//    private lateinit var textView: TextView
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        presenter = PresenterImpl(this@MainActivity)
+//
+//        textView = findViewById(R.id.text_view)
+//        textView.setOnClickListener { presenter.confirm() } //Presenter로 이벤트 전달
+//    }
+//
+//    // Presenter로 부터 전달받은 내용으로 View 업데이트
+//    override fun setText(text: String) {
+//        textView.text = text
+//    }
+//}
+
+//class MainActivity : AppCompatActivity() {
+//    private lateinit var binding: ActivityMainBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        binding.vm = ViewModel()
+//        binding.apply {
+//            lifecycleOwner = this@MainActivity
+//        }
+//    }
+//}
